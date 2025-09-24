e63a801b351bc3f3cbe43d90598e01bc1748898b
smmpanel434 <team.kenyayouthfederation@gmail.com>
2025-09-24 13:53:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./",   // 👈 ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
